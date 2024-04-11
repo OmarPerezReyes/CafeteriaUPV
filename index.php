26ce4787b08481137fad6090477a2576eab36c17
Adrian Ruiz <2130211@upv.edu.mx>
2024-04-10 21:58:14 -0600

    login

--- index.php
+++ index.php
@@ -4,49 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
-        body {
-            background-color: #f8f9fa;
+        a {
+            text-decoration: none;
         }
-        .login-container {
-            margin-top: 5%;
+        .login-page {
+            width: 100%;
+            height: 100vh;
+            display: inline-block;
+            display: flex;
+            align-items: center;
         }
-        .login-form {
-            background-color: #fff;
-            padding: 30px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+        .form-right i {
+            font-size: 100px;
         }
-        .login-header {
-            text-align: center;
-            margin-bottom: 30px;
+        /* Ajuste para hacer la imagen del logo más pequeña */
+        .form-right img {
+            max-width: 200px; /* Cambiar el valor según necesites */
+            width: 100%;
+            height: auto;
         }
     </style>
 </head>
 <body>
-    <div class="container login-container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 login-form">
-                <div class="login-header">
-                    <h2>Iniciar sesión</h2>
+<div class="login-page bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <h3 class="mb-3">Iniciar sesión</h3>
+                <div class="bg-white shadow rounded">
+                    <div class="row">
+                        <div class="col-md-7 pe-0">
+                            <div class="form-left h-100 py-5 px-5">
+                                <form action="./app/controllers/Login.php" method="post" class="row g-4">
+                                    <div class="col-12">
+                                        <label>Correo<span class="text-danger">*</span></label>
+                                        <div class="input-group">
+                                            <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
+                                            <input type="email" name="correo" class="form-control" placeholder="Correo electrónico" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <label>Contraseña<span class="text-danger">*</span></label>
+                                        <div class="input-group">
+                                            <div class="input-group-text"><i class="bi bi-lock-fill"></i></div>
+                                            <input type="password" name="contraseña" class="form-control" placeholder="Contraseña" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-primary px-4 float-end mt-4">Iniciar sesión</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-md-5 ps-0 d-none d-md-block">
+                            <div class="form-right h-100 bg-primary text-white text-center pt-5">
+                                <h2 class="fs-1">Jaguares con hambre</h2>
+                                <img src="public/images/logo.png" alt="" style="max-width: 200px;"> <!-- Ajuste en línea para el tamaño máximo -->
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <form action="./app/controllers/Login.php" method="post">
-                    <div class="form-group">
-                        <input type="email" name="correo" class="form-control" placeholder="Correo electrónico" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="contraseña" class="form-control" placeholder="Contraseña" required>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
