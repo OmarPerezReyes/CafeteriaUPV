129c6d4ed24caea6086fbd21d8252168a3a9b5ab
OmarPerezReyes <2130073@upv.edu.mx>
2024-04-11 08:00:23 +0000

    Agregar session en login y menu principal
    Carga de productos

--- app/views/productos.php
+++ app/views/productos.php
@@ -3,26 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>Menú de Productos</title>
+    <!-- Agregar la referencia al CSS de Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="carrito.php">Carrito</a></li>
-            <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="carrito.php">Carrito</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cerrar_sesion.php">Cerrar Sesión</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 
-    <h1>Menú de Productos</h1>
+    <div class="container">
+        <h1 class="mt-4">Menú de Productos</h1>
 
-    <div class="productos">
-        <?php foreach ($productos as $producto): ?>
-            <form action="agregar_al_carrito.php" method="post">
-                <button type="submit" name="agregar" value="<?php echo $producto['id']; ?>">
-                    <img src="<?php echo $producto['imagen_url']; ?>" alt="<?php echo $producto['nombre']; ?>" style="width: 50px; height: 50px;">
-                    <?php echo $producto['nombre']; ?> - $<?php echo $producto['precio']; ?>
-                </button>
-            </form>
-        <?php endforeach; ?>
+        <div class="row">
+            <?php foreach ($productos as $producto): ?>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <img src="<?php echo $producto['imagen_url']; ?>" class="card-img-top" alt="<?php echo $producto['nombre_Producto']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $producto['nombre_Producto']; ?></h5>
+                        <p class="card-text">$<?php echo $producto['precio']; ?></p>
+                        <form action="agregar_al_carrito.php" method="post">
+                            <input type="hidden" name="agregar" value="<?php echo $producto['id_producto']; ?>">
+                            <button type="submit" class="btn btn-primary">Agregar al Carrito</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
     </div>
+
+    <!-- Agregar la referencia al JS de Bootstrap (opcional) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
